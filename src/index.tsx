18008c0157dedf0c54ccf4714605e20504c3136c
emine ikuta <emine.ikuta@gmail.com>
2020-11-22 14:16:21 +0900

    filename to CamelCase

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { useReducer, useCallback } from 'react';
 import * as ReactDOM from 'react-dom';
-import { TodoState, Todo, todoReducer } from './todo-reducer';
+import { TodoState, Todo, todoReducer } from './todoReducer';
 
 type TodoProps = {
   todo: Todo;
